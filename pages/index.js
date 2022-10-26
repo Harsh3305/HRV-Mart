4285b49c121b90bb4d01d384df5daa2c5081c0d4
Harsh3305 <harshverma3305@gmail.com>
2022-10-26 23:59:10 +0530

    For debug purpose

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,23 @@
 import Categories from "../components/categories"
 import styles from "../styles/Home.module.css"
-import absoluteUrl from 'next-absolute-url'
 
 export default function Home({categories}) {
   console.log(categories)
-  const { origin } = absoluteUrl(req)
   return (
     <div className={styles.main}>
-      {/* {Categories(categories)} */}
-      {origin}
+      {Categories(categories)}
   </div>
   )
 }
 
-// export async function getServerSideProps() {
-//   const { origin } = absoluteUrl(req)
-//   const res = await fetch(`${origin}/api/categories`)
-//   const posts = await res.json()
-//   const categories = posts.categories;
-//   // console.log(categories)
-//   return {
-//     props: {
-//       categories
-//     },
-//   }
-// }
+export async function getServerSideProps() {
+  const res = await fetch(`/api/categories`)
+  const posts = await res.json()
+  const categories = posts.categories;
+  // console.log(categories)
+  return {
+    props: {
+      categories
+    },
+  }
+}
