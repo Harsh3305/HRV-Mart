55287b6c2248843cbef60154a96310b00eb4094f
Harsh3305 <harshverma3305@gmail.com>
2022-10-26 19:27:33 +0530

    Integrated login with backend

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,36 +1,62 @@
 import GoogleProvider from 'next-auth/providers/google'
 import NextAuth from 'next-auth';
+var axios = require('axios');
 
-export default NextAuth ({
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_SECRET
-      })
-    ],
-    debug: false,
-    session: {
-      maxAge: 30 * 24 * 60 * 60, // 30 days,
-      updateAge: 24 * 60 * 60, // 24 hours
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    })
+  ],
+  debug: false,
+  session: {
+    maxAge: 30 * 24 * 60 * 60, // 30 days,
+    updateAge: 24 * 60 * 60, // 24 hours
+  },
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const name = profile.name;
+      const picture = profile.picture;
+      const locale = profile.locale
+      /*
+       * Use name, picture and locale when integrating data  
+       */
+      var data = JSON.stringify({
+        "username": "mor_2314",
+        "password": "83r5^_"
+      });
+
+      var config = {
+        method: 'post',
+        url: 'https://fakestoreapi.com/auth/login',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data: data
+      };
+      var d ;
+      await axios(config)
+        .then(function (response) {
+          d = JSON.stringify(response.data);
+          // console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      // console.log("Loged in");
+      return true;
+      // }
     },
-    callbacks: {
-      async signIn({ user, account, profile, email, credentials }) {
-        console.log({profile: profile})
-        const name = profile.name;
-        const picture = profile.picture;
-        const locale = profile.locale
-        return true;
-        // }
-      },
-      async redirect({ url, baseUrl }) {
-        return baseUrl
-      },
-      async session({ session, user, token }) {
-        return session
-      },
-      async jwt({ token, user, account, profile, isNewUser }) {
-        return token
-      },
+    async redirect({ url, baseUrl }) {
+      return baseUrl
     },
-    secret: process.env.SECRET
+    async session({ session, user, token }) {
+      return session
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      return token
+    },
+  },
+  secret: process.env.SECRET
 })
