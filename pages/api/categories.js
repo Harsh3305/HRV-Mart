47ebe6830ab3c2a31d48cc305be79ac6fc030fd7
Harsh3305 <harshverma3305@gmail.com>
2022-10-26 23:08:38 +0530

    Added categories api

---
+++ pages/api/categories.js
@@ -0,0 +1,24 @@
+var axios = require('axios');
+
+export default async function handler(req, res) {
+    var config = {
+        method: 'get',
+        url: `${process.env.BACKEND_URL}/products/categories`,
+        headers: { }
+    };
+    
+    
+    await axios(config)
+    .then(function (response) {
+        // console.log(JSON.stringify(response.data));
+        // console.log(response.data);
+        res.status(200).json({"categories": response.data})
+    })
+    .catch(function (error) {
+        // console.log(error);
+        res.status(500).json({error: "Something went wrong!!"})
+        
+    });
+
+    
+}
