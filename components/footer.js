b82944385a5d6ada5ce50abaa03357586e538112
Harsh3305 <harshverma3305@gmail.com>
2022-10-26 20:19:06 +0530

    Updated Footer UI

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,13 @@
+import styles from "../styles/Footer.module.css";
+
 export default function Footer  () {
-    return <div>
-        Footer
+    return <div className={styles.background}>
+        <div className={styles.title}>
+            🛒 HRV-Mart
+        </div>
+        
+        <div className={styles.lower}>
+            This website is created with ❤️ by <a href="https://github.com/Harsh3305">Harsh Verma</a>
+        </div>
     </div>
 }
