066a226bdd0736328c4e6f0b5ebc70b328e35f18
Harsh3305 <harshverma3305@gmail.com>
2022-10-26 23:30:10 +0530

    Minor changes

--- components/categories.js
+++ components/categories.js
@@ -1,34 +1,31 @@
 import styles from "../styles/Categories.module.css"
-export default function Categories({ categories }) {
+export default function Categories( categories) {
+    console.log(categories.length);
     return (
         <div className={styles.main}>
             <div className={styles.categories}>
-                <div className={styles.category}>
-                    {categories[0]}
-                </div>
-                <div className={styles.category}>
-                    {categories[1]}
-                </div>
-                <div className={styles.category}>
-                    {categories[2]}
-                </div>
-                <div className={styles.category}>
-                    {categories[3]}
-                </div>
-                
+                {
+                    categories.map(category => (
+                        <div className={styles.category}>
+                            {category}
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
 }
-export async function getStaticProps() {
-    console.log("in categories");
-    const res = await fetch('/api/categories')
-    const posts = await res.json()
-    const categories = posts.categories;
+// export async function getStaticProps() {
+//     console.log("in categories");
+//     const res = await fetch('/api/categories')
+//     const posts = await res.json()
+//     console.warn(posts)
+//     const categories = posts.categories;
 
-    return {
-      props: {
-        categories
-      },
-    }
-}
+//     return {
+//       props: {
+//         categories
+//       },
+//     }
+// }
+  
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
 import Categories from "../components/categories"
 import styles from "../styles/Home.module.css"
-export default function Home({post}) {
+export default function Home({categories}) {
+  console.log(categories)
   return (
     <div className={styles.main}>
-      <Categories/>
+      {Categories(categories)}
+      {/* <h1>reoihgoeir</h1> */}
   </div>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:3000/api/categories')
+  const posts = await res.json()
+  const categories = posts.categories;
+  // console.log(categories)
+  return {
+    props: {
+      categories
+    },
+  }
 }
