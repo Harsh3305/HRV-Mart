4b098acee137f478ce391c0a97389f6a0bb6d822
Harsh3305 <harshverma3305@gmail.com>
2022-10-27 14:06:41 +0530

    Minor update

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,31 @@
 import Categories from "../components/categories"
+import ProductSection from "../components/product_section";
 import styles from "../styles/Home.module.css"
 
-export default function Home({categories}) {
+export default function Home({categories, products}) {
+  console.log(products)
   return (
     <div className={styles.main}>
-      {Categories(categories)}
+      {/* {Categories(categories)} */}
+      <ProductSection title={"Recommended for You"} products={products}/>
+      <ProductSection title={"Trending"} products={products}/>
+      <ProductSection title={"Based on your Search History"} products={products}/>
   </div>
   )
 }
 
 export async function getStaticProps() {
-  const res = await fetch(`${process.env.URL}/api/categories`)
-  const posts = await res.json()
-  const categories = posts.categories;
+  const categories = (await apiCall(`${process.env.URL}/api/categories`)).categories;
+  const products = await apiCall(`${process.env.URL}/api`)
     return {
     props: {
-      categories
+      categories,
+      products
     },
   }
+}
+async function apiCall (path) {
+  const res = await fetch(path)
+  const result = await res.json()
+  return result
 }
