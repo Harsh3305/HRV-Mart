e89867fc72ea48f9893c58650f248b4b663dde66
Harsh3305 <harshverma3305@gmail.com>
2022-10-27 14:06:04 +0530

    Added product section

---
+++ components/product_section.js
@@ -0,0 +1,17 @@
+import ProductOverview from "./product_overview";
+import styles from "../styles/ProductSection.module.css";
+
+export default function ProductSection ({title, products}) {
+    return <div>
+    <div className={styles.title}>
+      {title}
+    </div>
+    <div className={styles.products}>
+      {
+        products.map(
+          product => (<ProductOverview product={product} key={product.id}/>)
+        )
+      }
+    </div>
+  </div>
+}
