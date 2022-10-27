d0526bbaac387c1c01cbffb1ccb2be74578a55dc
Harsh3305 <harshverma3305@gmail.com>
2022-10-27 08:56:54 +0530

    Update categories

--- components/categories.js
+++ components/categories.js
@@ -1,31 +1,20 @@
+import Link from "next/link";
 import styles from "../styles/Categories.module.css"
-export default function Categories( categories) {
+export default function Categories(categories) {
     console.log(categories.length);
     return (
         <div className={styles.main}>
             <div className={styles.categories}>
                 {
                     categories.map(category => (
-                        <div className={styles.category} key={category}>
-                            {category}
-                        </div>
+                        <Link href={`/category/${category}`}>
+                            <a className={styles.category} key={category}>
+                                {category}
+                            </a>
+                        </Link>
                     ))
                 }
             </div>
         </div>
     )
 }
-// export async function getStaticProps() {
-//     console.log("in categories");
-//     const res = await fetch('/api/categories')
-//     const posts = await res.json()
-//     console.warn(posts)
-//     const categories = posts.categories;
-
-//     return {
-//       props: {
-//         categories
-//       },
-//     }
-// }
-  
