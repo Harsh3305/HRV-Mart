cfa5672dc2dfa02792bce6f5098d8a4dbe80d1ba
Harsh3305 <harshverma3305@gmail.com>
2022-10-27 08:57:18 +0530

    Updated cateogry

--- pages/category/[category].js
+++ pages/category/[category].js
@@ -1,11 +1,34 @@
 import { useRouter } from 'next/router'
+import ProductOverview from '../../components/product_overview';
+import styles from "../../styles/Category.module.css"
 
-export default function Category  () {
-    const router = useRouter()
-    const { category } = router.query;
+export default function Category({ products }) {
+  const router = useRouter()
+  const { category } = router.query;
 
-    return <div >
-        category {category}
+  return <div>
+    <div className={styles.title}>
+      Category : {category}
     </div>
+    <div className={styles.products}>
+      {
+        products.map(
+          product => (<ProductOverview product={product} />)
+        )
+      }
+    </div>
+  </div>
 
+}
+export async function getServerSideProps(content) {
+
+  const category = content.params.category;
+  const res = await fetch(`${process.env.URL}/api/category/${category}`)
+  const products = await res.json()
+
+  return {
+    props: {
+      products
+    },
+  }
 }
