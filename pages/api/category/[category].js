238e949709c369004fb9b7b23997ec4627d0b478
Harsh3305 <harshverma3305@gmail.com>
2022-10-27 07:16:53 +0530

    Added dynamic api route

--- pages/api/category/[category].js
+++ pages/api/category/[category].js
@@ -1,5 +1,6 @@
 export default async function handler (req, res) {
-    fetch(`${process.env.BACKEND_URL}/products/category/jewelery`)
+    const { category } = req.query
+    fetch(`${process.env.BACKEND_URL}/products/category/${category}`)
     .then(result=>result.json())
     .then(json=>res.status(500).json(json))
     .catch(error => res.status(500).json("Something went wrong!!"))
