d98bb09909d6c76be7f0764c8fe1fbbe0f47a23e
Harsh3305 <harshverma3305@gmail.com>
2022-10-28 17:05:28 +0530

    Changes done

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,5 @@
 export default async function hndler (req, res) {
-    fetch(`${process.env.BACKEND_URL}/products?limit=5`)
+    fetch(`${process.env.BACKEND_URL}/products`)
             .then(res=>res.json())
             .then(json=>res.status(200).json(json))
             .catch(error => res.status(500).json("Something went wrong"))
