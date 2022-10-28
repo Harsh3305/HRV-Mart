ae2b0fbdc873b9cfd5ecffbc5490f1644038d3d7
Harsh3305 <harshverma3305@gmail.com>
2022-10-28 15:58:47 +0530

    image loading

--- components/product_overview.js
+++ components/product_overview.js
@@ -1,14 +1,15 @@
 import Image from "next/image";
 import Router from "next/router";
 import styles from "../styles/ProductOverview.module.css";
-
+import loading from "../public/loading.png"
 export default function ProductOverview({ product }) {
     return <div className={styles.main} id={product.id} onClick={()=>(Router.push(`/product/${product.id}`))}>
         <div className={styles.title}>
             {product.title}
         </div>
         <div className={styles.image}>
-            <Image  src={product.image} height={400} width={400}/>
+            <Image  src={product.image} height={400} width={400} blurDataURL={loading}
+   placeholder="blur"/>
         </div>
         <div className={styles.price}>
             Price: ₹ {product.price}
