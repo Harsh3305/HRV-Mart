e5a5f5f423958ddde6ed9533523185c2ba6f6166
Harsh3305 <55652117+Harsh3305@users.noreply.github.com>
2022-11-25 17:22:57 +0530

    HMF-36-Change-to-address-breaking-change-in-backend (#38)
    * Done
    
    * For debug purpose
    
    * Remove log'
    
    * Select data from product. This will we remove in next pr
    
    * Minor changes

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,6 +1,6 @@
 export default async function hndler (req, res) {
     fetch(`${process.env.BACKEND_URL}/products`)
             .then(res=>res.json())
-            .then(json=>res.status(200).json(json))
+            .then(json=>res.status(200).json(json["data"]))
             .catch(error => res.status(500).json("Something went wrong"))
 }
