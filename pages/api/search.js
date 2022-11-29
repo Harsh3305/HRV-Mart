ee72157b0761609e30655680cd1d4fe4b17a81e1
Harsh3305 <harshverma3305@gmail.com>
2022-11-29 12:29:02 +0530

    Sync search with backend

---
+++ pages/api/search.js
@@ -0,0 +1,16 @@
+import { getSession } from "next-auth/react"
+export default async function handler(req, res) {
+    if (req.method == "GET") {
+
+        const query = req.query["query"]
+        const path = `${process.env.BACKEND_URL}/products/search?query=${query}`
+        console.log(path)
+        fetch(path)
+            .then(result => result.json())
+            .then(json => res.status(200).json(json))
+            .catch(error => res.status(500).send(error))
+    }
+    else {
+        res.status(404).send("Not found")
+    }
+}
