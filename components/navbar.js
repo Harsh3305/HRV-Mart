84ce635763507e21e29b4165d5c945df0897ded9
Harsh3305 <harshverma3305@gmail.com>
2022-11-29 13:06:54 +0530

    Link search bar with search page

--- components/navbar.js
+++ components/navbar.js
@@ -2,9 +2,21 @@ import styles from "../styles/NavBar.module.css";
 import Link from 'next/link'
 import { useSession, signIn, signOut } from "next-auth/react"
 import Router from "next/router";
+import { useState } from "react";
 
 export default function NavBar() {
     const { data: session } = useSession();
+    const [searchTest, setSearchTest] = useState("")
+
+    const handleKeyDown = event => {
+
+        if (event.key === 'Enter') {
+
+            console.log(searchTest);
+            Router.push(`/search/${searchTest}`)
+        }
+    };
+
     return <div className={styles.navbar}>
         <div className={styles.left}>
             <Link href="/">
@@ -15,6 +27,17 @@ export default function NavBar() {
             <input
                 placeholder="Search Products..."
                 className={styles.searchBar}
+                onChange={(event) => {
+                    console.log(event.target.value)
+                    setSearchTest(event.target.value)
+                    if (event.target.value.length >= 3) {
+                        Router.push(`/search/${event.target.value}`)
+                    }
+                }}
+                value={searchTest}
+                onKeyDown={
+                    handleKeyDown
+                }
             />
         </div>
         <div className={styles.right}>
