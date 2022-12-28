25c4065ac83917c0ab3a4b329eeff620dc1b80ef
Harsh3305 <harshverma3305@gmail.com>
2022-12-28 15:32:55 +0530

    Fixed user profile

--- pages/api/profile.js
+++ pages/api/profile.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
         res: res
     });
 
-    fetch(`${process.env.BACKEND_URL}/user`, {
+    fetch(`${process.env.BACKEND_URL}`, {
         headers: {
             "Authentication": `bearer:${token}`
         }
