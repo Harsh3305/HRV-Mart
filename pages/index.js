c2c48a75442d072c389d5a3065f9bf78d26fa401
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-01-12 20:29:35 +0530

    Temporary remove api calls

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,42 @@
+import { useState } from "react";
 import Categories from "../components/categories"
 import ProductSection from "../components/product_section";
 import styles from "../styles/Home.module.css"
 
-export default function Home({ categories, products }) {
+export default function Home() {
+  const [products, setProducts] = useState([]);
+  async function getProducts () {
+
+  }
   return (
     <div className={styles.main}>
-      {/*
-      * There is a bug in Categories. It is adding a scrollbar in home page.
-      */}
-      {Categories(categories)}
       <ProductSection title={"Recommended for You"} products={products} />
-      {/* <ProductSection title={"Trending"} products={products}/>
-      <ProductSection title={"Based on your Search History"} products={products}/> */}
     </div>
   )
-}
-
-export async function getStaticProps() {
-  const categories = (await apiCall(`${process.env.URL}/api/categories`)).categories;
-  console.log(categories);
-  const products = (await apiCall(`${process.env.URL}/api`))
-  console.log(products)
-  return {
-    props: {
-      categories,
-      products
-    },
-  }
-}
-async function apiCall(path) {
-  const res = await fetch(path)
-  try {
-    const result = await res.json();
-    return result;
-  }
-  catch (error) {
-    console.log({
-      path: path,
-      error: error
-    })
-    return [];
-  }
 
 }
+
+// export async function getStaticProps() {
+//   const categories = (await apiCall(`${process.env.URL}/api/categories`)).categories;
+//   const products = (await apiCall(`${process.env.URL}/api`))
+//   return {
+//     props: {
+//       categories,
+//       products
+//     },
+//   }
+// }
+// async function apiCall(path) {
+//   const res = await fetch(path)
+//   try {
+//     const result = await res.json();
+//     return result;
+//   }
+//   catch (error) {
+//     console.log({
+//       path: path,
+//       error: error
+//     })
+//     return [];
+//   }
+// }
