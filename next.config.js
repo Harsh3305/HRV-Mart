a9664f7ce83082279ebbccb4413eb1d5acb51640
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-01-12 16:32:28 +0530

    Minor change

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: [
-      "fakestoreapi.com", ,
+      "fakestoreapi.com",
       "api.lorem.space",
       "lh3.googleusercontent.com"
   ]
