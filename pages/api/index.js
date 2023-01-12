5793c8e6974cb49cc9dab0439f393a4d0f649f2f
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-01-12 20:30:50 +0530

    Minor chage

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,6 +1,6 @@
-export default async function hndler (req, res) {
+export default async function handler(req, res) {
     fetch(`${process.env.BACKEND_URL}/products`)
-            .then(res=>res.json())
-            .then(json=>res.status(200).json(json["data"]))
-            .catch(error => res.status(500).json("Something went wrong"))
+        .then(res => res.json())
+        .then(json => res.status(200).json(json))
+        .catch(error => res.status(500).json("Something went wrong"))
 }
